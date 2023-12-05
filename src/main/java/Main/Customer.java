fcc1ee0c9ff15f058650caf908e5795bfacd2fee
sorensuper10 <soren_super@hotmail.com>
2023-12-05 15:01:55 +0100

    Changes by Morten

--- src/main/java/Main/Customer.java
+++ src/main/java/Main/Customer.java
@@ -1,4 +1,91 @@
 package Main;
 
+import java.util.ArrayList;
+
 public class Customer {
+    private int costumerID;
+    private String costumerName, costumerLastName, costumerAddress, costumerPostalcode, costumerPhone, costumerMail;
+
+    private ArrayList<Customer> customerTabel = new ArrayList<Customer>();
+    public Customer() {
+    }
+
+    public Customer(int costumerID, String costumerName, String costumerLastName, String costumerAddress, String costumerPostalcode, String costumerPhone, String costumerMail) {
+        this.costumerID = costumerID;
+        this.costumerName = costumerName;
+        this.costumerLastName = costumerLastName;
+        this.costumerAddress = costumerAddress;
+        this.costumerPostalcode = costumerPostalcode;
+        this.costumerPhone = costumerPhone;
+        this.costumerMail = costumerMail;
+    }
+
+    public int getCostumerID() {
+        return costumerID;
+    }
+
+    public void setCostumerID(int costumerID) {
+        this.costumerID = costumerID;
+    }
+
+    public String getCostumerName() {
+        return costumerName;
+    }
+
+    public void setCostumerName(String costumerName) {
+        this.costumerName = costumerName;
+    }
+
+    public String getCostumerLastName() {
+        return costumerLastName;
+    }
+
+    public void setCostumerLastName(String costumerLastName) {
+        this.costumerLastName = costumerLastName;
+    }
+
+    public String getCostumerAddress() {
+        return costumerAddress;
+    }
+
+    public void setCostumerAddress(String costumerAddress) {
+        this.costumerAddress = costumerAddress;
+    }
+
+    public String getCostumerPostalcode() {
+        return costumerPostalcode;
+    }
+
+    public void setCostumerPostalcode(String costumerPostalcode) {
+        this.costumerPostalcode = costumerPostalcode;
+    }
+
+    public String getCostumerPhone() {
+        return costumerPhone;
+    }
+
+    public void setCostumerPhone(String costumerPhone) {
+        this.costumerPhone = costumerPhone;
+    }
+
+    public String getCostumerMail() {
+        return costumerMail;
+    }
+
+    public void setCostumerMail(String costumerMail) {
+        this.costumerMail = costumerMail;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "costumerID=" + costumerID +
+                ", costumerName='" + costumerName + '\'' +
+                ", costumerLastName='" + costumerLastName + '\'' +
+                ", costumerAddress='" + costumerAddress + '\'' +
+                ", costumerPostalcode='" + costumerPostalcode + '\'' +
+                ", costumerPhone='" + costumerPhone + '\'' +
+                ", costumerMail='" + costumerMail + '\'' +
+                '}';
+    }
 }
--- src/main/java/Main/DbSql.java
+++ src/main/java/Main/DbSql.java
@@ -18,5 +18,106 @@ public class DbSql {
             throwables.printStackTrace();
         }
     }
+
+    public ArrayList createPackage(Package p) {
+        try {
+            String sql = "insert into Package (packageID, packageSize, packageWeight, sender, reciever, finaldestination, sent, arrived)";
+            sql += "values (" + String.valueOf(p.getPackageID()) + ",'" + p.getSize() + "','" + p.getWeight();
+            sql += "','" + p.getSender() + "','" + p.getReciever() + "','" + p.getFinalDestination() + "',";
+            sql += String.valueOf(p.isSent()) + "," + String.valueOf(p.isArrived()) + ")";
+            Statement stmt = connection.createStatement();
+            stmt.execute(sql);
+            stmt.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+
+    public ArrayList createCustomer(Customer c) {
+        try {
+            String sql = "insert into Customer (customerID, customerName, CustomerLastName, customerAdress, customerPostalCode, customerPhone, customerMail)";
+            sql += "values (" + String.valueOf(c.getCostumerID()) + ",'" + c.getCostumerName() + "','" + c.getCostumerLastName();
+            sql += "','" + c.getCostumerAddress() + "','" + c.getCostumerPostalcode() + "','" + c.getCostumerPhone() + "',";
+            sql += "'" + (c.getCostumerMail()) + "'" + ")";
+            Statement stmt = connection.createStatement();
+            stmt.execute(sql);
+            stmt.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+
+    public ArrayList createCompany(Company co) {
+        try {
+            String sql = "insert into Company (companyID, companyName, companyAdress, companyPostalcode, companyPhone, companyMail, companyCVR)";
+            sql += "values (" + String.valueOf(co.getCompanyID()) + ",'" + co.getCompanyName() + "','" + co.getCompanyAddress();
+            sql += "','" + co.getCompanyPostalCode() + "','" + co.getCompanyPhone() + "','" + co.getCompanyCVR() + "',";
+            sql += "'" + (co.getCompanyMail()) + "'" + ")";
+            Statement stmt = connection.createStatement();
+            stmt.execute(sql);
+            stmt.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+
+    public ArrayList createLocation(Locations l) {
+        try {
+            String sql = "insert into Location (locationID, Adress)";
+            sql += "values (" + String.valueOf(l.getLocationID()) + ",'" + l.getAddress() + "'" + ")";
+            Statement stmt = connection.createStatement();
+            stmt.execute(sql);
+            stmt.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+
+    public ArrayList createTransportationInfo(TransportationInfo t) {
+        try {
+            String sql = "insert into Transportationinfo (ID, packageID, Destination, arrived)";
+            sql += "values (" + String.valueOf(t.getId()) + ",'" + t.getPackageID() + "','" + t.getDestination() + "',";
+            sql += String.valueOf(t.isArrived()) + ")";
+            Statement stmt = connection.createStatement();
+            stmt.execute(sql);
+            stmt.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+
+    public ArrayList udskrivPackages() {
+        ArrayList<Package> packagelist = new ArrayList<>();
+        try {
+            String sql = "select * from Package";
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                Package p = new Package();
+                p.setPackageID(rs.getInt("PackageID"));
+                p.setSize(rs.getDouble("packageSize"));
+                p.setWeight(rs.getDouble("packageWeight"));
+                p.setSender(rs.getString("Sender"));
+                p.setReciever(rs.getString("Reciever"));
+                p.setFinalDestination(rs.getString("FinalDestination"));
+                int x = rs.getInt("Sent");
+                boolean b = x == 1;
+                p.setSent(b);
+                int x1 = rs.getInt("Arrived");
+                b = x1 == 1;
+                p.setArrived(b);
+                packagelist.add(p);
+            }
+            stmt.close();
+        } catch (SQLException throwables){
+            throwables.printStackTrace();
+        }
+        return packagelist;
+    }
 }
 
