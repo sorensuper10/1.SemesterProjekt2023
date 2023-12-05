c3ced381961961ec779cde83efaba5d99bbe8720
sorensuper10 <soren_super@hotmail.com>
2023-12-05 15:01:44 +0100

    Changes by Diaco

--- src/main/java/Main/Package.java
+++ src/main/java/Main/Package.java
@@ -1,4 +1,108 @@
 package Main;
 
+import java.util.ArrayList;
+
 public class Package {
+private int packageID;
+private double size;
+private double weight;
+private String sender;
+private String reciever;
+private String finalDestination;
+private boolean sent;
+private boolean arrived;
+
+    private ArrayList<Package> packageTabel = new ArrayList<Package>();
+
+    public Package() {
+    }
+
+    public Package(int packageID, double size, double weight, String sender, String reciever, String finalDestination, boolean sent, boolean arrived) {
+        this.packageID = packageID;
+        this.size = size;
+        this.weight = weight;
+        this.sender = sender;
+        this.reciever = reciever;
+        this.finalDestination = finalDestination;
+        this.sent = sent;
+        this.arrived = arrived;
+    }
+
+    public int getPackageID() {
+        return packageID;
+    }
+
+    public void setPackageID(int packageID) {
+        this.packageID = packageID;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReciever() {
+        return reciever;
+    }
+
+    public void setReciever(String reciever) {
+        this.reciever = reciever;
+    }
+
+    public String getFinalDestination() {
+        return finalDestination;
+    }
+
+    public void setFinalDestination(String finalDestination) {
+        this.finalDestination = finalDestination;
+    }
+
+    public boolean isSent() {
+        return sent;
+    }
+
+    public void setSent(boolean sent) {
+        this.sent = sent;
+    }
+
+    public boolean isArrived() {
+        return arrived;
+    }
+
+    public void setArrived(boolean arrived) {
+        this.arrived = arrived;
+    }
+
+    @Override
+    public String toString() {
+        return "Package{" +
+                "packageID=" + packageID +
+                ", size=" + size +
+                ", weight=" + weight +
+                ", sender='" + sender + '\'' +
+                ", reciever='" + reciever + '\'' +
+                ", finalDestination='" + finalDestination + '\'' +
+                ", sent=" + sent +
+                ", arrived=" + arrived +
+                '}';
+    }
 }
--- src/main/java/Main/TransportationInfo.java
+++ src/main/java/Main/TransportationInfo.java
@@ -1,4 +1,64 @@
 package Main;
 
+import java.util.ArrayList;
+
 public class TransportationInfo {
+    private int id;
+    private int packageID;
+    private String destination;
+    private boolean arrived;
+
+    private ArrayList<TransportationInfo> transportationInfoTabel = new ArrayList<TransportationInfo>();
+
+    public TransportationInfo() {
+    }
+
+    public TransportationInfo(int id, int packageID, String destination, boolean arrived) {
+        this.id = id;
+        this.packageID = packageID;
+        this.destination = destination;
+        this.arrived = arrived;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPackageID() {
+        return packageID;
+    }
+
+    public void setPackageID(int packageID) {
+        this.packageID = packageID;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public boolean isArrived() {
+        return arrived;
+    }
+
+    public void setArrived(boolean arrived) {
+        this.arrived = arrived;
+    }
+
+    @Override
+    public String toString() {
+        return "TransportationInfo{" +
+                "id=" + id +
+                ", packageID=" + packageID +
+                ", destination='" + destination + '\'' +
+                ", arrived=" + arrived +
+                '}';
+    }
 }
